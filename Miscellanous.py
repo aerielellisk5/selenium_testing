962a206aeb7f882d12586efaf11569498fdd78a3
Aeriel <aerielellisk5@gmail.com>
2024-03-04 04:47:41 -0800

    screenshots and other miscellanous things

--- Miscellanous.py
+++ Miscellanous.py
@@ -1,15 +1,40 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("headless")
+# chrome_options.add_argument("--ignore-certificate-errors")
+
+# service_obj = Service("/Users/rahulshetty/documents/chromedriver")
+# driver = webdriver.Chrome(service=service_obj, options=chrome_options)
+# driver.implicitly_wait(2)
+
+# driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+# driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
+# driver.get_screenshot_as_file("screen.png")
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("headless")
-chrome_options.add_argument("--ignore-certificate-errors")
+options = webdriver.ChromeOptions()
 
-service_obj = Service("/Users/rahulshetty/documents/chromedriver")
-driver = webdriver.Chrome(service=service_obj, options=chrome_options)
-driver.implicitly_wait(2)
+# headmode vs headless mode --> headless will make sure that it won't open up the browser
+options.add_argument("headless")
+option.add_argument("--ignore-certificate-errors")
 
+
+options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+service_obj = Service("/opt/homebrew/bin/chromedriver")
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
-
+# scroll to the bottom of the page
 driver.execute_script("window.scrollBy(0,document.body.scrollHeight);")
-driver.get_screenshot_as_file("screen.png")
+
+# take a screenshot
+driver.get_screenshot_as_file("screen4.png")
+
--- framesDemo.py
+++ framesDemo.py
@@ -1,19 +1,34 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.firefox.service import Service
+
+# service_obj = Service("/Users/rahulshetty/documents/geckodriver")
+# driver = webdriver.Firefox(service=service_obj)
+# driver.implicitly_wait(2)
+
+# driver.get("https://the-internet.herokuapp.com/iframe")
+# driver.switch_to.frame("mce_0_ifr")
+# driver.find_element(By.ID,"tinymce").clear()
+# driver.find_element(By.ID,"tinymce").send_keys("I am able to automate frames")
+# driver.switch_to.default_content()
+# print(driver.find_element(By.CSS_SELECTOR,"h3").text)
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-service_obj = Service("/Users/rahulshetty/documents/geckodriver")
-driver = webdriver.Firefox(service=service_obj)
-driver.implicitly_wait(2)
-
+options = webdriver.ChromeOptions()
+options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+service_obj = Service("/opt/homebrew/bin/chromedriver")
+driver = webdriver.Chrome(service=service_obj, options=options)
 driver.get("https://the-internet.herokuapp.com/iframe")
 driver.switch_to.frame("mce_0_ifr")
-driver.find_element(By.ID,"tinymce").clear()
-driver.find_element(By.ID,"tinymce").send_keys("I am able to automate frames")
+driver.find_element(By.ID, "tinymce").clear()
+driver.find_element(By.ID, "tinymce").send_keys("I am able to automate frames")
 driver.switch_to.default_content()
-print(driver.find_element(By.CSS_SELECTOR,"h3").text)
-
-
+print(driver.find_element(By.CSS_SELECTOR, "h3").text)
 
 
 
